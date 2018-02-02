1705cc85c5465db4fc00caca6832f121c54e3745
Raghavendra.Damaraju <Raghavendra.Damaraju@IHOD31078.synechron.com>
2018-02-02 18:35:43 +0530

    Updated ReverseWord

--- JavaProjects/src/com/Synechron/JavaProject/ReverseWord.java
+++ JavaProjects/src/com/Synechron/JavaProject/ReverseWord.java
@@ -1,21 +1,40 @@
 package com.Synechron.JavaProject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class ReverseWord {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		String inputString="My Name is Raghavendra";
+	//String inputString="My Name is Raghavendra";
+	
+		/*Scanner s=new Scanner(System.in);
+		System.out.println("enter the value of s");
+		String inputString=s.next(s);*/
+		InputStreamReader in=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(in);
+	String inputString=br.readLine();
+		
 		String[] words = inputString.split(" ");
 		String result = "";
-		//System.out.println(words.length);
-		for (int i = words.length - 1; i >= 0; i--) {
+		System.out.println(words.length); 
+		for (int i = words.length-1; i >= 0; i--) {
 		 
 			result += (words[i]+"  ");
 			
 		}
-		System.out.println(result.trim());
+		/*System.out.println(result);
+		*/
+		for (int j = inputString.length()-1; j >= 0; j--) {
+			 
+			result += inputString.charAt(j);
+			
+		}
+		System.out.println(result);
 		
-
 		}
 		
 			
